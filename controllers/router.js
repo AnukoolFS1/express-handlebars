2845b7bd0bc2c94b8cd226b95d12b0a494856c92
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-03 18:44:06 +0530

    create update delete

--- controllers/router.js
+++ controllers/router.js
@@ -3,14 +3,19 @@ const persons = require('../models/person');
 
 
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.render('home')
+})
+router.get('/update', (req, res) => {
+    res.render('edit')
+})
+router.get('/delete', (req, res) => {
+    res.render('delete')
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    const {name, email, phone, password} = req.body;
-    
+    const { name, email, phone, password } = req.body;
+
     const data = new persons({
         name, email, phone, password
     })
@@ -20,6 +25,30 @@ router.post('/', (req, res) => {
         res.render('home')
     })
     // res.end()
+})
+
+router.post('/update', async (req, res) => {
+    const { name, email, phone, password } = req.body;
+
+    let result  = await persons.findOneAndUpdate({ name }, { email, phone, password })
+
+    if(!result){
+        res.status(400)
+        res.end('<h1>No Member Found</h1>')
+    }else{
+        res.render('edit')
+    }
+})
+
+router.post('/delete', async (req, res) => {
+    const name = req.body.name;
+
+    let result = await persons.deleteOne({name});
+    if(result){
+        res.render('delete');
+    }else{
+        res.status(400).json({msg:'member not found'})
+    }
 })
 
 module.exports = router
